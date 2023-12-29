577ce2e048276dbb827586ba99a354c8e3a67fd6
Joona Gynther <joona@gynther.xyz>
2023-12-29 16:25:03 +0200

    update to use sentencetransformers

--- embeddings.py
+++ embeddings.py
@@ -1,28 +1,15 @@
-from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer
 from utils import construct_embedding_string
 import torch
 import orjson
 
-
-model_id = "BAAI/bge-small-en-v1.5"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModel.from_pretrained(model_id)
-
+model_id = "llmrails/ember-v1"
+model = SentenceTransformer(model_id, device="mps")
 
 with open("oracle-cards.json") as file:
     data = orjson.loads(file.read())
 
-
 cards = [construct_embedding_string(card) for card in data]
-del data
 
-
-with open("embeddings.txt", "a") as file:
-    with torch.no_grad():
-        for card in cards:
-            inputs = tokenizer(card, padding=True, truncation=True, return_tensors="pt")
-            output = model(**inputs)
-            embedding = output[0][:, 0]
-            embedding = torch.nn.functional.normalize(embedding, p=2, dim=1)
-            embedding = embedding.squeeze().tolist()
-            file.write(f"{embedding}\n")
+embeddings = model.encode(cards, normalize_embeddings=True, show_progress_bar=True)
+torch.save(embeddings, "embeddings.pt")
--- utils.py
+++ utils.py
@@ -1,11 +1,25 @@
+import re
+
+
 def construct_embedding_string(card: dict[str, str]):
     type_line = card["type_line"]
-    # These might not exist
-    mana_cost = card.get("mana_cost", "")
     oracle_text = card.get("oracle_text", "")
-    power = card.get("power", "")
-    toughness = card.get("toughness", "")
 
-    string = f"{type_line} {mana_cost} {power} {toughness} {oracle_text}"
+    text = " ".join([type_line, oracle_text])
 
-    return string.replace("\n", "")
+    text = text.lower()
+    text = text.replace(card["name"].lower(), "this")  # Remove card name
+    text = re.sub(r"\([^)]+\)", "", text)  # Remove reminder text between ()
+    text = text.replace("\n", " ")  # Remove newlines
+
+    return text
+
+
+def find_card(name: str, cards: list[dict[str, str]]):
+    name = name.lower()
+
+    for i, card in enumerate(cards):
+        if card["name"].lower() == name:
+            return i
+
+    raise ValueError(f"Card {name} not found")
